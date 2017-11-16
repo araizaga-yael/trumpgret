8b364f4a5f87423970f395de84d64da406508a84
araizaga_yael <araizaga.yael@gmail.com>
2017-11-16 22:48:00 +0000

    Changed testing file to successfully get dates of original tweets from Trump_Regrets account

--- test.py
+++ test.py
@@ -13,7 +13,18 @@ api = tweepy.API(auth)
 
 # Using the API object to get tweets from your timeline, and storing it in a variable called public_tweets
 public_tweets = api.home_timeline()
+
+#print("\nMy tweets")
 # foreach through all tweets pulled
-for tweet in public_tweets:
-   # printing the text stored inside the tweet object
-   print(tweet.text)
+#for tweet in public_tweets:
+    # printing the text stored inside the tweet object
+    #if(tweet.retweeted)
+#    originaltTweet = api.get_status(tweet.id_str)
+#    print(originaltTweet.retweeted_status.created_at)
+
+print("\n\nTrumpgret")
+
+trumpgrets = api.user_timeline(screen_name = 'Trump_Regrets', count = 5, include_rts = True)
+for tweet in trumpgrets:
+    originaltTweet = api.get_status(tweet.id_str)
+    print(originaltTweet.retweeted_status.created_at)
